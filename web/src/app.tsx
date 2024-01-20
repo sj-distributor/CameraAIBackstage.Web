22d3037ae8e489e09dde7b83238d0cb0653c49ab
Aimer <w08260826w@gmail.com>
2024-01-20 11:22:17 +0800

    Update code

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,4 @@
 import { ConfigProvider } from "antd";
-import zhCN from "antd/lib/locale/zh_CN";
 import { BrowserRouter } from "react-router-dom";
 
 import AuthProvider from "./hooks/auth";
@@ -8,7 +7,6 @@ import { Router } from "./routes";
 function App() {
   return (
     <ConfigProvider
-      locale={zhCN}
       theme={{ token: { colorPrimary: "#2853E3", colorText: "#323444" } }}
     >
       <BrowserRouter>
