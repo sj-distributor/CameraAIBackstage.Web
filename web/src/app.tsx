b2b574a815beeaad56255932776b186e93e56d64
KOKI <249054278@qq.com>
2024-02-03 06:56:00 +0800

    Update app.tsx

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -11,10 +11,7 @@ function App() {
 
   useEffect(() => {
     // 内部调试自行加个 token 值上去
-    localStorage.setItem(
-      (window as any).appSettings?.tokenKey,
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFkbWluIiwibmFtZWlkIjoiMSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2F1dGhlbnRpY2F0aW9uIjoiU2VsZiIsInJvbGUiOiJBZG1pbmlzdHJhdG9yIiwibmJmIjoxNzA0ODc2NzAwLCJleHAiOjE3MDQ4ODAzMDAsImlhdCI6MTcwNDg3NjcwMH0.PZjHoHGsJ5O4oX9RABvk6ICMKYSu9-HuqTzPTuZJ5m4"
-    );
+    localStorage.setItem((window as any).appSettings?.tokenKey, "");
   }, [localStorage.getItem((window as any).appSettings?.tokenKey)]);
 
   useEffect(() => {
