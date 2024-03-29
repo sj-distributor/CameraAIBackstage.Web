cb1a692bf69a786e189837281d109db2153542d3
TRACY.W <463002899@qq.com>
2024-03-29 18:30:22 +0800

    update

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -12,6 +12,12 @@ function App() {
   const [aPageData, setAPageData] = useState<string>("");
 
   useEffect(() => {
+    console.log(
+      "isLoaded",
+      isLoaded,
+      aPageData,
+      (window as any).appSettings?.tokenKey
+    );
     if (isLoaded) {
       localStorage.setItem((window as any).appSettings?.tokenKey, aPageData);
     }
@@ -20,6 +26,8 @@ function App() {
   useEffect(() => {
     const aPageData = localStorage.getItem("aPageData");
     if (aPageData) {
+      console.log("aPageData", aPageData);
+
       setAPageData(aPageData);
       localStorage.removeItem("aPageData");
     } else {
@@ -27,6 +35,8 @@ function App() {
     }
 
     function receiveMessage(event: { origin: string; data: string }) {
+      console.log("event.data", event.data);
+
       if (event.origin !== (window as any).appSettings?.frontDeskDomain) return;
       if (event.data) {
         localStorage.setItem("aPageData", event.data);
