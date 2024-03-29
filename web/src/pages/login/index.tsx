5c0e38736fc28cbd6e710e25cde28a39f965b41a
KOKI <249054278@qq.com>
2024-03-29 22:51:18 +0800

    Update index.tsx

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -11,7 +11,7 @@ export const Login = () => {
     <div className="w-screen h-screen bg-[#2866F1] flex justify-end md:justify-normal overflow-hidden no-scrollbar">
       <div className="w-3/5 h-full hidden min-[1200px]:block">
         <img
-          src="../src/assets/login.png"
+          src={loginImage}
           alt=""
           className="w-full h-full bg-cover img-no-darg"
         />
