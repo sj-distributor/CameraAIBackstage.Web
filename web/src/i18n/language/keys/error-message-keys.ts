055d7c094306d60aa00676cced40cb38328817f9
Mindy.L <mindy.li@sjfood.com>
2024-08-30 14:18:17 +0800

    delete no use

--- web/src/i18n/language/keys/error-message-keys.ts
+++ web/src/i18n/language/keys/error-message-keys.ts
@@ -1,11 +1,6 @@
 export default {
   DUPLICATE_CAMERA: "DuplicateCamera",
   DATABASE_ERROR: "DatabaseError",
+  ERROR_CAMERA: "ErrorCamera",
   INSUFFICIENT_RESOURCES: "InsufficientResources",
-  CAMERA_INFO_ERROR: "CameraInfoError",
-  CAMERA_CONNECTION_FAILED: "CameraConnectionFailed",
-  VIDEO_STREAM_ERROR: "VideoStreamError",
-  TASK_PARAMETER_ERROR: "TaskParameterError",
-  REPLAY_STREAM_ERROR: "ReplayStreamError",
-  PROGRAM_ERROR: "ProgramError",
 };
