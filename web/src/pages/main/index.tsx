7284ef1de96c68e307624d63c7d24d6c4325d393
chenmeiling12 <108569295+chenmeiling12@users.noreply.github.com>
2024-11-12 16:43:49 +0800

    fixed

--- web/src/pages/main/index.tsx
+++ web/src/pages/main/index.tsx
@@ -40,7 +40,6 @@ export const Container = () => {
 
   return (
     <div className="pt-6 px-6 flex-1 flex flex-col bg-[#F6F8FC] overflow-hidden">
-      <Outlet />
       {permission.isGetPermission ? (
         permission.hasSwitchCameraAiBackEnd ? (
           <Outlet />
